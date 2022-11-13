c65aadaae1c9b7accf2f1e8a89f22f31aa215f41
Homero José <96498455+rox651@users.noreply.github.com>
2022-11-13 10:13:07 -0500

    useFormCrud changed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,14 +4,22 @@ import { GrClose } from "react-icons/gr";
 
 import { useFormCrud } from "../hooks/useFormCrud";
 
+const initialForm = {
+  username: "",
+  state: "Activo",
+  email: "",
+  type: "Admin",
+  select: false,
+};
+
 export const Form = () => {
   //redux
   const { isOpenForm, userEdit } = useSelector((state) => state.crud);
-  const { form, userName, email, onSubmitForm, onHandleChange, closeForm } = useFormCrud();
+  const { username, email, onInputChange, onResetForm, closeForm } = useFormCrud(initialForm);
 
   return (
     <div className={clsx(isOpenForm ? "grid" : "hidden", "absolute w-full h-screen bg-[#1f293770] place-items-center")}>
-      <form className="bg-white h-[350px] w-[350px] relative p-10" onSubmit={onSubmitForm} ref={form}>
+      <form className="bg-white h-[350px] w-[350px] relative p-10" onSubmit={onResetForm}>
         <header className="flex justify-between items-center">
           <h2 className="text-slate-600 text-2xl font-bold">Formulario</h2>
           <GrClose className=" cursor-pointer" onClick={closeForm} />
@@ -19,19 +27,19 @@ export const Form = () => {
         <div className="flex flex-col justify-evenly h-full">
           <input
             className="border-2 px-3 py-2"
-            onChange={onHandleChange}
+            onChange={onInputChange}
             type="text"
             name="username"
             placeholder="Nombre de usuario"
-            ref={userName}
+            value={username}
           />
           <input
             className="border-2 px-3 py-2"
-            onChange={onHandleChange}
+            onChange={onInputChange}
             type="email"
             name="email"
             placeholder="Email"
-            ref={email}
+            value={email}
           />
           <input
             className="bg-gray-800 text-white px-3 py-2 rounded-md text-sm cursor-pointer"
--- src/hooks/useFormCrud.js
+++ src/hooks/useFormCrud.js
@@ -4,56 +4,49 @@ import { v4 as uuid } from "uuid";
 
 import { openForm, addData, setUserEdit, editData } from "../store/slices/CRUD";
 
-export const useFormCrud = () => {
+export const useFormCrud = (initialForm = {}) => {
   //redux
   const { userEdit } = useSelector((state) => state.crud);
   const dispatch = useDispatch();
 
-  //form
-  const initialUser = {
-    username: "",
-    state: "Activo",
-    email: "",
-    type: "Admin",
-    select: false,
-  };
-  const form = useRef();
-  const userName = useRef();
-  const email = useRef();
+  const [formState, setFormState] = useState(initialForm);
 
-  const [user, setUser] = useState(initialUser);
-
-  //condition
   const isUserNull = userEdit.id !== null;
-
-  //functions
 
   useEffect(() => {
     if (isUserNull) {
-      userName.current.value = userEdit.username;
-      email.current.value = userEdit.email;
+      setFormState({
+        ...formState,
+        username: userEdit.username,
+        email: userEdit.email,
+      });
     }
   }, [userEdit]);
 
-  const onHandleChange = (e) => {
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+
+    console.log(value);
+
     if (isUserNull) {
       dispatch(
         setUserEdit({
           ...userEdit,
-          [e.target.name]: e.target.value,
+          [name]: value,
         }),
       );
       return;
     }
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
+
+    setFormState({
+      ...formState,
+      [name]: value,
     });
   };
 
-  const onSubmitForm = (e) => {
+  const onResetForm = (e) => {
     e.preventDefault();
-    const isEmpty = [userName.current.value, email.current.value].includes("");
+    const isEmpty = [formState.username, formState.email].includes("");
 
     if (isEmpty) {
       alert("Completa todos los campos");
@@ -65,14 +58,13 @@ export const useFormCrud = () => {
       closeForm();
       return;
     }
-
     dispatch(
       addData({
         id: uuid(),
-        ...user,
+        ...formState,
       }),
     );
-    setUser(initialUser);
+    setFormState(initialForm);
     closeForm();
   };
 
@@ -86,8 +78,14 @@ export const useFormCrud = () => {
     }
 
     dispatch(openForm());
-    form.current.reset();
+    setFormState(initialForm);
   };
 
-  return { form, userName, email, onHandleChange, onSubmitForm, closeForm };
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+    closeForm,
+  };
 };
